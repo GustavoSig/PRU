d370fa88dd9d01e10434bdf7611a300bdb31f35c
GustavoSig <67558997+GustavoSig@users.noreply.github.com>
2020-07-17 23:16:50 -0400

    update aap.js

--- aap.js
+++ aap.js
@@ -3,3 +3,11 @@ let viajando = destino => `viajando a la ciudad de : ${destino}`;
 let viaje;
 viaje = viajando('Paris');
 console.log(viaje);
+
+function saludar(nombre){
+  console.log('bienvenido '+ nombre);
+}
+
+saludar('Gustavo');
+saludar('jose');
+saludar('kot')
