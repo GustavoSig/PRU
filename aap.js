4493b5200edf0e86254249df825f67536c6c8325
Gustavo <gustavo_sig@hotmail.com>
2020-07-18 23:16:20 -0400

    practica javascrp

--- aap.js
+++ aap.js
@@ -7,7 +7,34 @@ console.log(viaje);
 function saludar(nombre){
   console.log('bienvenido '+ nombre);
 }
-
 saludar('Gustavo');
 saludar('jose');
-saludar('kot')
+saludar('kot');
+
+const cliente = function(nombreCliente){
+    console.log('mostrando datos del cliente: '+ nombreCliente);
+}
+cliente('juan');
+cliente('Gustavo');
+// esto es igual a
+let cliente1=nombreCliente=>console.log(`mostrando datos del cliente: ${nombreCliente}`);
+cliente1('das');
+
+
+
+function actividad (nombre,actividad){
+    console.log(`la persona ${nombre}, esta realizando la actividad ${actividad}`);
+}
+actividad('gustavo','Aprender Javascript');
+function actividadDefaukt (nombre='walter white',actividad='enseñar quimica'){
+  console.log(`la persona ${nombre}, esta realizando la actividad ${actividad}`);
+}
+actividadDefaukt();
+
+const aprendiendoJS={
+  version:{
+    
+  }
+}
+
+
